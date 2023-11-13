da56953661fec780eb1d57c577027c10cda9c21f
kiharito <kihara13611.c7d7@gmail.com>
2023-11-13 21:59:23 +0900

    典型90問:004

--- typical90/src/bin/004.rs
+++ typical90/src/bin/004.rs
@@ -1,6 +1,26 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (h, w): (usize, usize),
+        matrix: [[i32; w]; h]
+    }
+
+    let mut row_sums = vec![0; h];
+    let mut col_sums = vec![0; w];
+    for i in 0..h {
+        for j in 0..w {
+            row_sums[i] += matrix[i][j];
+            col_sums[j] += matrix[i][j];
+        }
+    }
+    for i in 0..h {
+        let mut row = vec![0; w];
+        for j in 0..w {
+            row[j] = row_sums[i] + col_sums[j] - matrix[i][j];
+        }
+        println!("{}", row.iter().join(" "));
+    }
 }
