30a4c7f1698980e661c2060d46ec09203959535e
kiharito <kihara13611.c7d7@gmail.com>
2023-11-13 22:23:27 +0900

    典型90問:022

--- typical90/src/bin/022.rs
+++ typical90/src/bin/022.rs
@@ -2,5 +2,9 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (a, b, c): (usize, usize, usize)
+    }
+    let gcd = num::integer::gcd(a, num::integer::gcd(b, c));
+    println!("{}", a / gcd + b / gcd + c / gcd - 3);
 }
