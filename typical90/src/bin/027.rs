13469e827f0e4677e2486b16a045875ba8261d3e
kiharito <kihara13611.c7d7@gmail.com>
2023-11-14 20:17:35 +0900

    典型90問:027

--- typical90/src/bin/027.rs
+++ typical90/src/bin/027.rs
@@ -1,6 +1,17 @@
 use proconio::{fastout, input};
+use std::collections::HashMap;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        n: usize,
+        s: [String; n]
+    }
+    let mut cache: HashMap<String, i32> = HashMap::new();
+    for (i, user) in s.iter().enumerate() {
+        if cache.get(user).is_none() {
+            println!("{}", i + 1);
+            cache.insert(user.to_string(), 1);
+        }
+    }
 }
