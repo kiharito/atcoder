12ee4984227fcb8a4eed7470ac417c9e5d1a5730
kiharito <kihara13611.c7d7@gmail.com>
2023-11-14 20:08:01 +0900

    典型90問:024

--- typical90/src/bin/024.rs
+++ typical90/src/bin/024.rs
@@ -2,5 +2,18 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, k): (usize, i32),
+        a: [i32; n],
+        b: [i32; n]
+    }
+    let mut diff_total = 0;
+    for i in 0..n {
+        diff_total += (a[i] - b[i]).abs();
+    }
+    if k >= diff_total && (k - diff_total) % 2 == 0 {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
 }
