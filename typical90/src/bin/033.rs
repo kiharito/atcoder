97314ca4df3b1a00e71b2ad38bf6cf265c5edd05
kiharito <kihara13611.c7d7@gmail.com>
2023-11-16 22:04:53 +0900

    典型90問:033

--- typical90/src/bin/033.rs
+++ typical90/src/bin/033.rs
@@ -2,5 +2,13 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (h, w): (usize, usize)
+    }
+
+    if h == 1 || w == 1 {
+        println!("{}", h * w);
+        return;
+    }
+    println!("{}", (((h + 1) / 2) * ((w + 1) / 2)));
 }
