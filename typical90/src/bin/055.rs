18dadff80863fb8cb8bfe2cc188a0db919c84552
kiharito <kihara13611.c7d7@gmail.com>
2023-11-17 21:59:46 +0900

    典型90問:055

--- typical90/src/bin/055.rs
+++ typical90/src/bin/055.rs
@@ -1,6 +1,23 @@
+use itertools::Itertools;
 use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, p, q): (usize, i64, i64),
+        a: [i64; n]
+    }
+
+    let mut ans = 0;
+    for nums in a.iter().combinations(5) {
+        let mut prod = 1;
+        for num in &nums {
+            prod *= *num % p;
+            prod %= p;
+        }
+        if prod % p == q {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
