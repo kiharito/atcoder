d2e543f555e26c896d6e3fd41cdac8a1f08763c5
kiharito <kihara13611.c7d7@gmail.com>
2023-11-18 10:49:43 +0900

    典型90問:061

--- typical90/src/bin/061.rs
+++ typical90/src/bin/061.rs
@@ -1,6 +1,21 @@
+use bitvec::macros::internal::funty::Fundamental;
 use proconio::{fastout, input};
+use std::collections::VecDeque;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        q: usize
+    }
+    let mut queue: VecDeque<i64> = VecDeque::from([]);
+    for _ in 1..=q {
+        input! { (t, x): (i32, i64) }
+        if t == 1 {
+            queue.push_front(x);
+        } else if t == 2 {
+            queue.push_back(x);
+        } else {
+            println!("{}", queue.get((x - 1).as_usize()).unwrap());
+        }
+    }
 }
