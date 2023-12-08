aa857155889d814ea31f2103913a8922eebd044f
kiharito <kihara13611.c7d7@gmail.com>
2023-12-08 23:45:52 +0900

    典型90問:067

--- typical90/src/bin/067.rs
+++ typical90/src/bin/067.rs
@@ -2,5 +2,31 @@ use proconio::{fastout, input};
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, k): (String, i32)
+    }
+    let mut num = i64::from_str_radix(&n, 8).unwrap();
+    for _ in 0..k {
+        let mut nine = shinsu(num, 9);
+        nine = nine.replace('8', "5");
+        num = i64::from_str_radix(&nine, 8).unwrap();
+    }
+    println!("{:o}", num);
+}
+
+// x(10進数)をn進数表記の文字列に変換する
+fn shinsu(x: i64, n: i64) -> String {
+    if x == 0 {
+        return "0".to_string();
+    }
+
+    let mut remainders: Vec<String> = Vec::new();
+    let mut num = x;
+    while num > 0 {
+        remainders.push((num % n).to_string());
+        num /= n;
+    }
+
+    remainders.reverse();
+    remainders.join("")
 }
