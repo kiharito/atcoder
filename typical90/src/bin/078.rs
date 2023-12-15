b4f46707b71815e897d3f65c327642dce7233a73
kiharito <kihara13611.c7d7@gmail.com>
2023-12-15 22:33:21 +0900

    典型90問:078

--- typical90/src/bin/078.rs
+++ typical90/src/bin/078.rs
@@ -1,6 +1,30 @@
+use im_rc::HashMap;
 use proconio::{fastout, input};
+use std::cmp;
 
 #[fastout]
 fn main() {
-    input! {}
+    input! {
+        (n, m): (i32, i32)
+    }
+    // keyより小さい隣接頂点の個数をvalueとしてもつ
+    let mut graph: HashMap<i32, i32> = HashMap::new();
+    for i in 1..=n {
+        graph.insert(i, 0);
+    }
+
+    for _ in 0..m {
+        input! {(a, b): (i32, i32)}
+        let max = cmp::max(a, b);
+        let cnt = *graph.get(&max).unwrap();
+        graph.insert(max, cnt + 1);
+    }
+
+    let mut ans = 0;
+    for (_, v) in graph {
+        if v == 1 {
+            ans += 1;
+        }
+    }
+    println!("{}", ans);
 }
